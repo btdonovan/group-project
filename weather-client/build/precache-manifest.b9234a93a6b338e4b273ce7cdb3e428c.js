1321beaed4bc80621b13a55ef6cad41c430b3786
Benjamin Todd Donovan <carcel@navonod.us>
2020-07-09 11:27:43 -0600

    Added build files to git

---
+++ weather-client/build/precache-manifest.b9234a93a6b338e4b273ce7cdb3e428c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07039c754cf000fda6286570e3717b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f8efde476d358d7b76",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "67f40a0cd569dcb5fc49",
+    "url": "/static/css/main.d3300cf6.chunk.css"
+  },
+  {
+    "revision": "48f8efde476d358d7b76",
+    "url": "/static/js/2.429937f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.429937f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f40a0cd569dcb5fc49",
+    "url": "/static/js/main.ceff110a.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
